add0ea02ba2293a41876d84544f921cfd8ac9406
Erin Burton <eb@flat9.dev>
2022-02-04 21:15:49 -0800

    updates

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,6 @@
-import type { StressLevelType } from '~/stressLevel'
-import { ActionFunction, Link, useCatch } from 'remix'
+import type { ActionFunction } from 'remix'
+import { StressLevel, StressLevelType } from '~/stressLevel'
+import { json, Link, useActionData, useCatch } from 'remix'
 import { LoaderFunction, useLoaderData, Form } from 'remix'
 import {
   MAX_STRESS_LEVEL,
@@ -8,31 +9,34 @@ import {
 } from '~/stressLevel'
 import { getStore, setStore } from '~/stressLevelProvider.server'
 import { useClasses } from '~/hooks'
+import * as Slider from '@radix-ui/react-slider'
+import { useEffect, useState } from 'react'
 
-const TYPE = 'type'
+type ActionData = {
+  message: string
+  level: 'good' | 'warning' | 'danger'
+}
+
 const LEVEL = 'level'
-const MORE_TYPE = 'more'
-const LESS_TYPE = 'less'
+const goodRequest = (data: ActionData) => json(data, { status: 201 })
+const badRequest = (data: ActionData) => json(data, { status: 400 })
 
 export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData()
   const level = form.get(LEVEL)
-  const type = form.get(TYPE)
 
-  if (typeof level !== 'string' || typeof type !== 'string') {
+  if (typeof level !== 'string') {
     throw new Response('form not submitted correctly', { status: 422 })
   }
 
   const numLevel = Number.parseInt(level, 10)
 
   if (Number.isNaN(numLevel)) {
-    throw new Response('unable to parse level', { status: 422 })
+    return badRequest({ message: 'Unable to parse level!', level: 'danger' })
   }
 
-  if (type === MORE_TYPE) {
-    return await setStore(numLevel + DEFAULT_STRESS_STEP)
-  }
-  return await setStore(numLevel - DEFAULT_STRESS_STEP)
+  await setStore(numLevel)
+  return goodRequest({ message: 'Saved!', level: 'good' })
 }
 
 export const loader: LoaderFunction = async () => {
@@ -43,7 +47,11 @@ const classFromLevel = (level: number, prefix: 'bg' | 'fg') =>
   prefix + '-' + (level < 33 ? 'good' : level < 66 ? 'warning' : 'danger')
 
 export default function Index() {
-  const sl = useLoaderData<StressLevelType>()
+  const data = useLoaderData<StressLevelType>()
+  const actionData = useActionData<ActionData>()
+
+  const [sl, setSL] = useState(data)
+  const [showMessage, setShowMessage] = useState(false)
 
   const containerClasses = useClasses(
     'container',
@@ -55,6 +63,13 @@ export default function Index() {
     classFromLevel(sl.level, 'fg'),
   )
 
+  useEffect(() => {
+    if (actionData?.message) {
+      setShowMessage(true)
+      setTimeout(() => setShowMessage(false), 3000)
+    }
+  }, [actionData, setShowMessage])
+
   return (
     <div className={containerClasses}>
       <main className='stress-o-meter'>
@@ -63,40 +78,46 @@ export default function Index() {
           <p className='site-subheading'>How stressed is my Kippums?</p>
         </header>
         <section className='stress-meter-container'>
+          <Slider.Root
+            className='stress-meter'
+            min={MIN_STRESS_LEVEL}
+            max={MAX_STRESS_LEVEL}
+            value={[sl.level]}
+            step={DEFAULT_STRESS_STEP}
+            onValueChange={([value]) => setSL(StressLevel(value))}
+          >
+            <Slider.Track className='stress-meter-track'>
+              <Slider.Range
+                className={`stress-meter-range ${classFromLevel(
+                  sl.level,
+                  'bg',
+                )}`}
+              />
+            </Slider.Track>
+            <Slider.Thumb className='stress-meter-thumb'>
+              <div className='stress-meter-thumb-percent'>{sl.level}%</div>
+            </Slider.Thumb>
+          </Slider.Root>
+          <header className='stress-header'>
+            <small className='stress-header-small'>Current stress level:</small>
+            <h2 className={descriptionClasses}>{sl.description}</h2>
+          </header>
           <Form className='stress-form' method='post'>
             <input name={LEVEL} value={sl.level} type='hidden' />
-            <meter
-              className='stress-meter'
-              high={69}
-              optimum={20}
-              min={MIN_STRESS_LEVEL}
-              max={MAX_STRESS_LEVEL}
-              value={sl.level}
-            ></meter>
-            <header className='stress-header'>
-              <small className='stress-header-small'>
-                Current stress level:
-              </small>
-              <h2 className={descriptionClasses}>
-                {sl.description} ({sl.level})
-              </h2>
-            </header>
-            <button
-              className='stress-meter-button bg-good'
-              type='submit'
-              name={TYPE}
-              value={LESS_TYPE}
-            >
-              Less stressed
-            </button>
-            <button
-              className='stress-meter-button bg-danger'
-              type='submit'
-              name={TYPE}
-              value={MORE_TYPE}
-            >
-              More stressed
-            </button>
+            <div className='stress-form-button'>
+              <button className='stress-meter-button bg-primary' type='submit'>
+                Lock in stress level
+              </button>
+              {showMessage ? (
+                <p
+                  className={`stress-form-message`}
+                  role='alert'
+                  id='save-message'
+                >
+                  {actionData?.message}
+                </p>
+              ) : null}
+            </div>
           </Form>
         </section>
       </main>
--- app/stressLevel.ts
+++ app/stressLevel.ts
@@ -4,16 +4,27 @@ export const DEFAULT_STRESS_LEVEL = 10
 export const DEFAULT_STRESS_STEP = 5
 
 const stressLevelDescriptions = [
+  'Literally squirting',
+  'Crookus kisses',
+  'Picnic with Master',
   'Cuddled up, with a pillow on head',
+  'Doordash came earlier than expected',
+  'Nice warm shower',
   'Pretty, relaxed, and cute',
   'Not very stressed',
   'Slightly tense',
+  'Season finale of Project Runway',
   'A bit sad and preoccupied',
   'Tail is thwomping a bit',
   'Whiskers are a little twitchy',
   'Whiskers are pulled a bit too hard',
+  'Bitcoin all-time low',
+  'Period time times',
+  'Home2 sold out, IU basketball night, New Jersey Hilton-Honors Diamond Member sorority mom yelling about the hole in her wall that her husband punched',
+  "Master's wifi went out",
   'The power is out, it is storming, and the heat is not working',
-  'The NFT wallet transfer is not working',
+  'The NTF wallet transfer is not working',
+  'Having Covid and a flat-time',
 ]
 
 export type StressLevelType = {
@@ -22,7 +33,7 @@ export type StressLevelType = {
 }
 
 function getDescriptionForStressLevel(level: number) {
-  return stressLevelDescriptions[Math.floor(level / 10 - 1)] || 'Not stressed'
+  return stressLevelDescriptions[level / 5] || 'Not stressed'
 }
 
 export function StressLevel(level: number) {
